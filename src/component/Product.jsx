2dd6391ac00ddb4955f78c54bdb8b3eb3136c523
harisangar <ganambihai10@gmail.com>
2022-09-06 17:21:30 +0530

    components and axios call

---
+++ src/component/Product.jsx
@@ -0,0 +1,33 @@
+import React,{ useState,useEffect} from 'react';
+import { useParams} from 'react-router';
+
+const Product=()=> {
+    const {id} = useParams();
+    const [product,setProduct]=useState([]);
+    const [loading,setLoading] = useState(false);
+
+    useEffect(()=>{
+        const getProduct = async ()=>{
+            setLoading(true);
+            const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+            setProduct(await response.json());
+            setLoading(false);
+        }
+        getProduct();
+    },[input]);
+    const Loading=()=>{
+        return(
+            <>
+           Loading....
+            </>
+        )
+    }
+  return (
+    <div>
+
+      
+    </div>
+  )
+}
+
+export default Product
